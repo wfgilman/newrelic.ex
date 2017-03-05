7ab40a027ba3d6dce076859ba6eaeabf4ece6c8e
Will Gilman <will@samadhi.tech>
2017-03-05 16:35:12 -0700

    Debugging...

--- lib/new_relic/repo.ex
+++ lib/new_relic/repo.ex
@@ -4,6 +4,7 @@ defmodule NewRelic.Repo do
   """
 
   def record_metrics(%Ecto.LogEntry{query: query, query_time: time}) do
+    IO.inspect "Received query"
     NewRelic.Collector.record_value({:db_query, query}, time)
   end
 end
